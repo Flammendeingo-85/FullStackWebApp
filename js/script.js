d485b7f7727d995e8d3c41386cf01ddeaee95fc7
R Banwari <Ravi.Banwari1108@gmail.com>
2024-07-14 23:45:01 +0200

    experimenting with some javasctip-code in script.js

--- js/script.js
+++ js/script.js
@@ -9,8 +9,38 @@ btn.addEventListener("click", function () {
     btn.textContent = "Close";
     // then remove the 'hidden'-class from this class, so the form becomes visible on the page
   } else {
-    form.classList.add("hidden"); 
+    form.classList.add("hidden");
     btn.textContent = "Share a fact";
     // else add the 'hidden'-class to the .fact-form class, so it becomes hidden again.
   }
 });
+
+let votesInteresting = 25;
+let votesMindBlowing = 23;
+
+if (votesInteresting === votesMindBlowing) {
+  alert("This fact is equally intersting and mindblowing");
+} else if (votesInteresting < votesMindBlowing) {
+  console.log("Interesting fact");
+} else if (votesInteresting > votesMindBlowing) {
+  console.log("votesInteresting Wins again");
+}
+
+//falsy values: 0, '', NULL, UNDEFINED
+if (votesMindBlowing) {
+  console.log("Just a Mindblowing fact!");
+} else votesMindBlowing < votesInteresting;
+{
+  console.log("Yep That's all Folks1");
+}
+
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+
+  if (age >= 0) return age;
+  else return "Impossible year";
+}
+
+console.log(calcFactAge(1985));
